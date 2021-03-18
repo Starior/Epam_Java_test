42f752eef5d136ab58fa4a209f1adc0556475ded
Starior <zzzartem@gmai.com>
2021-03-18 18:44:16 +0300

    task 4 through strings

--- src/com/starion/loki/basics/linear/Task_4.java
+++
@@ -1,7 +0,0 @@
-package com.starion.loki.basics.linear;
-
-public class Task_4 {
-    public static void main(String[] args) {
-        double num = 424.982;
-    }
-}
---
+++ src/com/starion/loki/basics/linear/Task_4_string.java
@@ -0,0 +1,14 @@
+package com.starion.loki.basics.linear;
+
+public class Task_4_string {
+    public static void main(String[] args) {
+        double num = 424.982;
+
+        // intermediate string
+        // String stringNum =  String.join(".",Double.toString(num).substring(4), Double.toString(num).substring(0, 3));
+
+        double numNew = Double.parseDouble(String.join(".", Double.toString(num).substring(4), Double.toString(num).substring(0, 3)));
+
+        System.out.println(numNew);
+    }
+}
