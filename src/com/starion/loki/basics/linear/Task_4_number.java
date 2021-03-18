807f1fe268aeb8af06f0ae21395e6a54f762bbdc
Starior <zzzartem@gmai.com>
2021-03-18 19:02:46 +0300

    task 4 through numbers

---
+++ src/com/starion/loki/basics/linear/Task_4_number.java
@@ -0,0 +1,16 @@
+package com.starion.loki.basics.linear;
+
+public class Task_4_number {
+    public static void main(String[] args) {
+        double num = 424.982;
+
+//        int first = (int) num;
+//        double firstDb = (double) first / 1000;
+//        double second = (int)(num % 1 * 1000);
+//        double numNew = second + firstDb;
+
+        double numNew = (int) (num % 1 * 1000) + (((double) ((int) num)) / 1000);
+
+        System.out.println(numNew);
+    }
+}
