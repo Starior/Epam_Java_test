4717846980c83eda61dbc207668f5771369ad597
Starior <zzzartem@gmai.com>
2021-03-23 18:20:58 +0300

    add branching & Task_1

---
+++ src/com/starion/loki/basics/com/branching/Task_1.java
@@ -0,0 +1,15 @@
+package com.starion.loki.basics.com.branching;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        byte a = 47;
+        byte b = 43;
+
+        if (a + b < 180) {
+            System.out.println("Такой треугольник существует");
+            if (a == 90 || b == 90 || 180 - a - b == 90)
+                System.out.println("Этот треугольник прямоугольный");
+        } else
+            System.out.println("Такой треугольник не существует");
+    }
+}
