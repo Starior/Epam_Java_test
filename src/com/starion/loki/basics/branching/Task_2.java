9118b423a05a11d6fbbebf22e33a5ea70bd811c0
Starior <zzzartem@gmai.com>
2021-03-23 18:31:11 +0300

    branching Task_2

---
+++ src/com/starion/loki/basics/branching/Task_2.java
@@ -0,0 +1,13 @@
+package com.starion.loki.basics.branching;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 324;
+        int c = 352;
+        int d = 232;
+
+        System.out.println(Math.max(Math.min(a,b), Math.min(c,d)));
+        
+    }
+}
