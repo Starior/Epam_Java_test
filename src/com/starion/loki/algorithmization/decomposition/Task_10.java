01e578234f51d7724dbebb70774d922ea9a2d742
Starior <zzzartem@gmai.com>
2021-04-10 12:48:27 +0300

    algorithmization decomposition t10

---
+++ src/com/starion/loki/algorithmization/decomposition/Task_10.java
@@ -0,0 +1,30 @@
+package com.starion.loki.algorithmization.decomposition;
+
+import java.util.Arrays;
+
+public class Task_10 {
+    public static void main(String[] args) {
+        int n = 1890105;
+        NumberToArray(n);
+    }
+
+    public static int countingLengthArray(int n) {
+        int i = 10;
+        int countNumbers = 1;
+        while (n != n % i) {
+            i *= 10;
+            countNumbers++;
+        }
+        return countNumbers;
+    }
+
+    public static void NumberToArray(int n) {
+        int[] array = new int[countingLengthArray(n)];
+        for (int j = array.length - 1; j >= 0; j--) {
+            array[j] = n % 10;
+            n = (n - array[j]) / 10;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+}
