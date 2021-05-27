b5e8fe4319e5ccfcb12b8e063df85a14ce4f8c78
Starior <zzzartem@gmai.com>
2021-05-27 19:02:39 +0300

    upgrade readme complete algorithmization.decomposition fix1

--- src/com/starion/loki/algorithmization/decomposition/Task_14.java
+++ src/com/starion/loki/algorithmization/decomposition/Task_14.java
@@ -1,14 +1,10 @@
 package com.starion.loki.algorithmization.decomposition;
 
-/*Натуральное число, в записи которого n цифр, называется числом Армстронга, если сумма его цифр,
-возведенная в степень n, равна самому числу. Найти все числа Армстронга от 1 до k.
-Для решения задачи использовать декомпозицию.*/
-
 /*A natural number with n digits in it is called the Armstrong number if the sum of its digits
     raised to the power n is equal to the number itself. Find all Armstrong numbers from 1 to k. To
     solve the task, use decomposition.*/
+/*Mistake?*/
 
-/*Ошибка описания числа Армстронга?*/
 public class Task_14 {
 
   public static void main(String[] args) {
